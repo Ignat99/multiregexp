07fd475a4b9721e3b1ca4662aceb45081a5b3fe8
doron.podoleanu <doron.podoleanu@sizmek.com>
2014-06-08 16:47:39 +0300

    comments

--- src/test/java/multiregexpbulks/RegexpsMachineTest.java
+++ src/test/java/multiregexpbulks/RegexpsMachineTest.java
@@ -5,22 +5,25 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
+
 public class RegexpsMachineTest {
 
     private static RegexpsMachineHelper<String> readFileAndConstructRegexpsMachineHelper(String regexpsFileName) {
+
         FileReader fr;
         BufferedReader br;
         AtomicReference<RegexpsMachineHelper<String>> regexpsMachineHelper;
         regexpsMachineHelper = new AtomicReference<RegexpsMachineHelper<String>>();
         regexpsMachineHelper.set(new RegexpsMachineHelper<String>());
-
         try {
             fr = new FileReader(regexpsFileName);
             br = new BufferedReader(fr);
@@ -38,8 +41,8 @@ public class RegexpsMachineTest {
         return regexpsMachineHelper.get();
     }
 
-
     private static Map<String, String> readTestSetFromFile(String regexpsFileName) {
+
         FileReader fr;
         Map<String, String> map = new LinkedHashMap<String, String>();
         BufferedReader br;
@@ -47,7 +50,6 @@ public class RegexpsMachineTest {
             fr = new FileReader(regexpsFileName);
             br = new BufferedReader(fr);
             String line;
-
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(";;");
                 map.put(parts[0], parts[1]);
@@ -62,14 +64,15 @@ public class RegexpsMachineTest {
 
     @Test
     public void test1() {
-        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper("src\\test\\java\\multiregexpbulks\\RegExps.txt");
+
+        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper
+                ("src\\test\\java\\multiregexpbulks\\RegExps.txt");
         Map<String, String> map = readTestSetFromFile("src\\test\\java\\multiregexpbulks\\TestSet.txt");
         boolean passed = true;
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String expected = entry.getValue();
             String actual = regexpsMachineHelper.getValueForURL(entry.getKey());
             String url = entry.getKey();
-
             if (!expected.equals(actual)) {
                 passed = false;
                 System.out.println("Category for " + url + " is " + actual);
@@ -81,7 +84,9 @@ public class RegexpsMachineTest {
 
     @Test
     public void test2() {
-        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper("src\\test\\java\\multiregexpbulks\\RegExps.txt");
+
+        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper
+                ("src\\test\\java\\multiregexpbulks\\RegExps.txt");
         Map<String, String> map = readTestSetFromFile("src\\test\\java\\multiregexpbulks\\TestSet.txt");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String expected = entry.getValue();
@@ -94,7 +99,9 @@ public class RegexpsMachineTest {
 
     @Test
     public void test3() {
-        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper("src\\test\\java\\multiregexpbulks\\RegExps.txt");
+
+        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper
+                ("src\\test\\java\\multiregexpbulks\\RegExps.txt");
         Map<String, String> map = readTestSetFromFile("src\\test\\java\\multiregexpbulks\\TestSet.txt");
         long start = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
@@ -107,7 +114,9 @@ public class RegexpsMachineTest {
 
     @Test
     public void test4() {
-        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper("src\\test\\java\\multiregexpbulks\\RegExps.txt");
+
+        RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper
+                ("src\\test\\java\\multiregexpbulks\\RegExps.txt");
         Map<String, String> map = readTestSetFromFile("src\\test\\java\\multiregexpbulks\\TestSet.txt");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String expected = entry.getValue();
@@ -115,6 +124,23 @@ public class RegexpsMachineTest {
             String url = entry.getKey();
             System.out.println("Category for " + url + " is " + actual);
             System.out.println("Expected " + expected);
+        }
+    }
+
+    @Test
+    public void test5() {
+
+        try {
+            RegexpsMachineHelper<String> regexpsMachineHelper = readFileAndConstructRegexpsMachineHelper
+                    ("src\\test\\java\\multiregexpbulks\\RegExps.txt");
+            FileOutputStream fileOut = new FileOutputStream("src\\test\\java\\multiregexpbulks\\serTest.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(regexpsMachineHelper);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /tmp/employee.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
         }
     }
 
